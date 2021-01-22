a94c4c669cdc3a5409f6a4ad724654178a200cf3
ryndovaira <ryndovaira@gmail.com>
2021-01-22 23:03:40 +0300

    Add test for pokemon class

---
+++ tests/pokemon_test.py
@@ -0,0 +1,18 @@
+from pokemon import Pokemon, Gender
+
+
+def test_init():
+    name = "Pika"
+    gender = Gender.MALE
+    pokemon = Pokemon(name=name, gender=gender)
+    assert pokemon.name == name
+    assert pokemon.gender == gender
+    assert pokemon.level == 0
+
+
+def test_str():
+    name = "Pika"
+    gender = Gender.MALE
+    pokemon = Pokemon(name=name, gender=gender)
+
+    assert str(pokemon) == f"Name: {name} | Gender: {gender} | Level: 0"
