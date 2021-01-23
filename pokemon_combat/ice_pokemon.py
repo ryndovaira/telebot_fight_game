379bd76191726bf2e1f64fc8194a86670713adef
ryndovaira <ryndovaira@gmail.com>
2021-01-23 14:03:38 +0300

    Update ice pokemon class

--- pokemon_combat/ice_pokemon.py
+++ pokemon_combat/ice_pokemon.py
@@ -1,7 +1,12 @@
 from pokemon_combat.pokemon import Pokemon, Gender
+from pokemon_combat.pokemon_types import PokemonType
 
 
 class IcePokemon(Pokemon):
     def __init__(self, name: str, gender: Gender):
         super().__init__(name, gender)
-        ...
+
+        self.weakness = (PokemonType.FIRE, PokemonType.STEEL, PokemonType.FIGHTING, PokemonType.ROCK)
+
+    def __str__(self):
+        return f"Type: Fire | {super().__str__()}"
