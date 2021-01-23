538e7eb25d748f1da8f0424942a66a9066fd6869
ryndovaira <ryndovaira@gmail.com>
2021-01-23 13:52:41 +0300

    Update fire pokemon class

--- pokemon_combat/fire_pokemon.py
+++ pokemon_combat/fire_pokemon.py
@@ -1,7 +1,14 @@
 from pokemon_combat.pokemon import Pokemon, Gender
+from pokemon_combat.pokemon_types import PokemonType
 
 
 class FirePokemon(Pokemon):
     def __init__(self, name: str, gender: Gender):
         super().__init__(name, gender)
-        ...
+
+        self.weakness = (PokemonType.WATER, )
+        self.strength = (PokemonType.ICE, PokemonType.GROUND, PokemonType.ROCK)
+
+    def __str__(self):
+        return f"Type: Fire | {super().__str__()}"
+
