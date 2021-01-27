0cf6034c13882203f5249a0c2af2d6489508f04c
ryndovaira <ryndovaira@gmail.com>
2021-01-27 20:15:16 +0300

    Add pokemon types and their advantages mock

---
+++ pokemon_combat/pokemon_types_advantages.py
@@ -0,0 +1,23 @@
+from pokemon_combat.pokemon_types import PokemonType
+
+pokemon_advantage_by_type = {
+    PokemonType.NORMAL: (),
+    PokemonType.FIRE: (),
+    PokemonType.WATER: (),
+    PokemonType.ELECTRIC: (),
+    PokemonType.GRASS: (),
+    PokemonType.ICE: (),
+    PokemonType.FIGHTING: (),
+    PokemonType.POISON: (),
+    PokemonType.GROUND: (),
+    PokemonType.FLYING: (),
+    PokemonType.PSYCHIC: (),
+    PokemonType.BUG: (),
+    PokemonType.ROCK: (),
+    PokemonType.GHOST: (),
+    PokemonType.DRAGON: (),
+    PokemonType.DARK: (),
+    PokemonType.STEEL: (),
+    PokemonType.FAIRY: ()
+}
+
