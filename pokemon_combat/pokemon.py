8eec11f2432a07315b798ac1c0b37b75957ae0ea
ryndovaira <ryndovaira@gmail.com>
2021-01-27 20:22:32 +0300

    Add pokemon type to pokemon class

--- pokemon_combat/pokemon.py
+++ pokemon_combat/pokemon.py
@@ -6,12 +6,19 @@ from pokemon_combat.pokemon_type import PokemonType
 
 
 class Pokemon:
-    def __init__(self, name: str, gender: Gender) -> None:
+    def __init__(self, name: str, gender: Gender, pokemon_type: PokemonType) -> None:
         self.level = 0  # одна победа = +1 уровень
         self.name = name  # имя покемона
         self.gender = gender  # пол
+        self.type = pokemon_type  # тип покемона (вода, огонь ...)
         self.hp = 100  # здоровье или очки жизни (hit points)
-        self.state = State.INIT
+        self.state = State.INIT  # текущее состояние (начальное, удар, защита)
 
     def __str__(self):
-        return f"Name: {self.name} | Gender: {self.gender} | Level: {self.level} | HP: {self.hp}"
+        return f"Name: {self.name} | Type: {self.type} | Gender: {self.gender} | Level: {self.level} | HP: {self.hp}"
+
+    def hit(self):
+        return randint(self.level + 1, self.level + 5) + (1 if self.gender == Gender.MALE else 0)
+
+    def defense(self):
+        return randint(self.level + 1, self.level + 5) + (1 if self.gender == Gender.FEMALE else 0)
--- tests/pokemon_test.py
+++ tests/pokemon_test.py
@@ -8,9 +8,11 @@ from pokemon_combat.pokemon import Pokemon
 def test_init():
     name = "Pika"
     gender = Gender.MALE
-    pokemon = Pokemon(name=name, gender=gender)
+    pokemon_type = PokemonType.ELECTRIC
+    pokemon = Pokemon(name=name, gender=gender, pokemon_type=pokemon_type)
     assert pokemon.name == name
     assert pokemon.gender == gender
+    assert pokemon.type == pokemon_type
     assert pokemon.level == 0
     assert pokemon.hp == 100
     assert pokemon.state == State.INIT
@@ -19,6 +21,8 @@ def test_init():
 def test_str():
     name = "Pika"
     gender = Gender.MALE
-    pokemon = Pokemon(name=name, gender=gender)
+    pokemon_type = PokemonType.ELECTRIC
 
-    assert str(pokemon) == f"Name: {name} | Gender: {gender} | Level: 0 | HP: 100"
+    pokemon = Pokemon(name=name, gender=gender, pokemon_type=pokemon_type)
+
+    assert str(pokemon) == f"Name: {name} | Type: {pokemon_type} | Gender: {gender} | Level: 0 | HP: 100"
