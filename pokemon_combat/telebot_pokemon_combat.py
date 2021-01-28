170fdd14c6be9abf44dd5e2803900e02c8d09c3d
ryndovaira <ryndovaira@gmail.com>
2021-01-28 15:22:54 +0300

    User can choose pokemon type and name

--- pokemon_combat/telebot_pokemon_combat.py
+++ pokemon_combat/telebot_pokemon_combat.py
@@ -2,6 +2,8 @@ import configparser
 import telebot
 from telebot import types
 
+from pokemon_combat.pokemon import Pokemon
+from pokemon_combat.pokemon_bot import PokemonBot
 from pokemon_combat.pokemon_type import PokemonType
 
 config = configparser.ConfigParser()
@@ -22,7 +24,7 @@ def new_member(message):
     yes_no_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True,
                                                 one_time_keyboard=True,
                                                 row_width=2)
-    yes_no_keyboard.row("Да", "Нет")
+    yes_no_keyboard.row("Yes", "No")
     bot.send_message(message.chat.id,
                      "Do you want to start the game?",
                      reply_markup=yes_no_keyboard)
@@ -31,7 +33,7 @@ def new_member(message):
 
 
 def start_game(message):
-    if message.text == 'Да':
+    if message.text == 'Yes':
         pokemon_markup = types.InlineKeyboardMarkup()
         for pokemon_type in PokemonType:
             pokemon_markup.add(types.InlineKeyboardButton(text=pokemon_type.name,
@@ -40,7 +42,7 @@ def start_game(message):
                          "Choose a pokemon type:",
                          reply_markup=pokemon_markup)
 
-    elif message.text == 'Нет':
+    elif message.text == 'No':
         bot.send_message(message.chat.id,
                          "Ok, I'll wait!",
                          reply_markup=hideBoard)
@@ -48,14 +50,36 @@ def start_game(message):
         bot.send_message(message.chat.id,
                          "You must choose option on keyboard")
 
-    bot.register_next_step_handler(message, start_game)
-
 
 # True можно заменить на любое логическое условие (можно использовать данные из объекта call)
 @bot.callback_query_handler(func=lambda call: "pokemon_type_" in call.data)
 def query_handler(call):
     bot.answer_callback_query(callback_query_id=call.id,
-                              text='You choose a type!')
+                              text="Let's create your pokemon!")
+
+    bot.send_message(call.message.chat.id,
+                     "Write your pokemon name:")  # TODO: Choose pokemon from the catalog
+
+    pokemon_type_id = int(call.data.split('_')[2])  # TODO: Check type and errors
+    bot.register_next_step_handler(call.message, user_pokemon_name, pokemon_type=PokemonType(pokemon_type_id))
+
+
+def user_pokemon_name(message, pokemon_type):
+    global bot_state
+    # {user_id: {'user_pokemon': pokemon_obj, 'rand_pokemon': rand_player_obj}}
+    bot_state[message.from_user.id] = {
+        'user_pokemon': Pokemon(name=message.text,
+                                pokemon_type=pokemon_type),
+        'rand_pokemon': PokemonBot()
+    }
+
+    bot.send_message(message.chat.id,
+                     f"<b>You choose</b>\n{bot_state[message.from_user.id]['user_pokemon']}",
+                     parse_mode='html')
+
+    bot.send_message(message.chat.id,
+                     f"<b>Bot choose</b>\n{bot_state[message.from_user.id]['rand_pokemon']}",
+                     parse_mode='html')
     ...
 
 
