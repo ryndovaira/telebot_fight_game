4651231010a97ce1cfbbb68297ef00c680ed280b
ryndovaira <ryndovaira@gmail.com>
2021-01-28 15:01:37 +0300

    Add body part min and max index getters

--- pokemon_combat/body_part.py
+++ pokemon_combat/body_part.py
@@ -2,7 +2,15 @@ from enum import Enum, auto
 
 
 class BodyPart(Enum):
-    NOTHING = auto()    # ничего (начальное состояние)
-    HEAD = auto()       # голова
-    BELLY = auto()      # живот
-    LEGS = auto()       # ноги
+    NOTHING = auto()  # ничего (начальное состояние)
+    HEAD = auto()  # голова
+    BELLY = auto()  # живот
+    LEGS = auto()  # ноги
+
+    @classmethod
+    def min_index(cls):
+        return cls.HEAD.value
+
+    @classmethod
+    def max_index(cls):
+        return cls.LEGS.value
