bd79a3cd9a7e44439f3822c2525d821ae487ddf1
ryndovaira <ryndovaira@gmail.com>
2021-01-31 22:29:13 +0300

    Add some pokemons by type

---
+++ pokemon_combat/pokemon_by_type.py
@@ -0,0 +1,65 @@
+from pokemon_combat.pokemon_type import PokemonType
+
+pokemon_defence_weaknesses_by_type = {
+    # https://pokemondb.net/type/normal
+    PokemonType.NORMAL: {'Snorlax': 'snorlax.jpg',     # https://pokemondb.net/pokedex/snorlax
+                          'Meowth': 'meowth.jpg'},     # https://pokemondb.net/pokedex/meowth
+
+    # https://pokemondb.net/type/fire
+    PokemonType.FIRE: {'Charmander': 'charmander.jpg',  # https://pokemondb.net/pokedex/charmander
+                       'Vulpix': 'vulpix.jpg'},         # https://pokemondb.net/pokedex/vulpix
+
+    # https://pokemondb.net/type/water
+    PokemonType.WATER: {'Squirtle': 'squirtle.jpg',     # https://pokemondb.net/pokedex/squirtle
+                        'Goldeen': 'goldeen.jpg'},      # https://pokemondb.net/pokedex/goldeen
+
+    # https://pokemondb.net/type/electric
+    PokemonType.ELECTRIC: {'Pikachu': 'pikachu.jpg',    # https://pokemondb.net/pokedex/pikachu
+                           'Jolteon': 'jolteon.jpg'},   # https://pokemondb.net/pokedex/jolteon
+
+    # https://pokemondb.net/type/grass
+    PokemonType.GRASS: {'Chikorita': 'chikorita.jpg',   # https://pokemondb.net/pokedex/chikorita
+                        'Leafeon': 'leafeon.jpg'},      # https://pokemondb.net/pokedex/leafeon
+
+
+    # https://pokemondb.net/type/ice
+    PokemonType.ICE: {'Regice': 'regice.jpg',           # https://pokemondb.net/pokedex/regice
+                      'Beartic': 'beartic.jpg'},        # https://pokemondb.net/pokedex/beartic
+
+    # https://pokemondb.net/type/fighting
+    PokemonType.FIGHTING: {'Primeape': 'primeape.jpg',  # https://pokemondb.net/pokedex/primeape
+                           'Throh': 'throh.jpg'},       # https://pokemondb.net/pokedex/throh
+
+    # https://pokemondb.net/type/poison
+    PokemonType.POISON: {},
+
+    # https://pokemondb.net/type/ground
+    PokemonType.GROUND: {},
+
+    # https://pokemondb.net/type/flying
+    PokemonType.FLYING: {},
+
+    # https://pokemondb.net/type/psychic
+    PokemonType.PSYCHIC: {},
+
+    # https://pokemondb.net/type/bug
+    PokemonType.BUG: {},
+
+    # https://pokemondb.net/type/rock
+    PokemonType.ROCK: {},
+
+    # https://pokemondb.net/type/ghost
+    PokemonType.GHOST: {},
+
+    # https://pokemondb.net/type/dragon
+    PokemonType.DRAGON: {},
+
+    # https://pokemondb.net/type/dark
+    PokemonType.DARK: {},
+
+    # https://pokemondb.net/type/steel
+    PokemonType.STEEL: {},
+
+    # https://pokemondb.net/type/fairy
+    PokemonType.FAIRY: {}
+}
